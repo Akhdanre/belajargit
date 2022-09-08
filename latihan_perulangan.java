91e4c1273e3ee4a016d07ef836c32eaa0f0bb086
Akhdanre <oukenzeumasio@gmail.com>
2022-09-08 19:45:07 +0700

    upload pertama

---
+++ latihan_perulangan.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lathian;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class latihan_perulangan {
+    public static void main (String [] args){
+        int a = 5;
+        int b = 2; 
+        int n = 10;
+    }
+}
